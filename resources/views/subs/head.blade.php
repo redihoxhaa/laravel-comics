ee6598ecc3b42f928c047f6b90dd79cfb8b1fcf5
redihoxhaa <redi.hoxha1996@gmail.com>
2024-01-28 18:57:45 +0100

    feat: aggiunta favicon

--- resources/views/subs/head.blade.php
+++ resources/views/subs/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}">
     <title>Brand new template</title>
     @vite('resources/js/app.js')
 </head>
